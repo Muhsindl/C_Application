97fad7ecc47e04bc7e0cc88ef4270671bdc7e021
Muhsin DOLU <muhsindolu06@gmail.com>
2024-02-28 19:44:33 +0300

    Hafta uygulaması
    Kullanıcı tarafından girilen gün değerine göre hafta içi veya hafta sonu olduğunu yazdıran uygulama

---
+++ HaftaIciveHaftasonuYazdirma.c
@@ -0,0 +1,27 @@
+//Ortak SwitchCase Uygulaması
+#include <stdio.h>
+
+int main()
+{
+    int gun;
+    printf("Günün kaçıncı gün olduğunu giriniz(1-7): ");
+    scanf("%d",&gun);
+    
+    switch(gun){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            printf("Hafta içi");
+            break;
+        case 6:
+        case 7:
+            printf("Hafta sonu");
+            break;
+        default:
+            printf("Geçersiz değer giriniz (1-7 arasında sayı giriniz)");
+            break;
+    }
+    return 0;
+}
