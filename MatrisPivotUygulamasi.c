20725b27ec7be03c21314d20beb38ee46c8a8d6a
Muhsin DOLU <muhsindolu06@gmail.com>
2024-05-06 18:43:36 +0300

    Kullanıcının istediği boyutta matris oluşturup pivot değerleri bir yana uygulama
    Kullanıcı tarafından girilen boyut sayısına göre oluşturulan bir matriste pivot elemanları bir yapıp diğer elemanları sıfır yapan uygulama

---
+++ MatrisPivotUygulamasi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int boyut;
+    // Kullanıcıdan boyut bilgisini alalım
+    printf("Boyut giriniz: ");
+    scanf("%d",&boyut);
+    //Diziyi oluşturalım
+    int array[boyut][boyut];
+    //Diziyi dolduralım
+    for(int i=0;i<boyut;i++){
+        for(int j=0; j<boyut;j++){
+            if(i==j){
+                array[i][j]=1;
+            }else{
+                array[i][j]=0;
+            }
+        }
+    }
+    //Diziyi ekrana yazdıralım
+    for(int i=0;i<boyut;i++){
+        for(int j=0; j<boyut;j++){
+            printf("%d",array[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
