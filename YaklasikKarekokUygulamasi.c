eaf720b018992a218f3670bcbe2c680ff731075b
Muhsin DOLU <muhsindolu06@gmail.com>
2024-06-28 23:52:03 +0300

    Update YaklasikKarekokUygulamasi.c

--- YaklasikKarekokUygulamasi.c
+++ YaklasikKarekokUygulamasi.c
@@ -5,9 +5,12 @@ int main()
 {
     int sayi;
     float yaklasikKarekok=0;
+    //Sayı alınır
     printf("Lütfen sayı giriniz: ");
     scanf("%d",&sayi);
+    // Karekök hesaplanır
     yaklasikKarekok = pow(sayi, 0.5);
+    // Ekranda gösterme
     printf("Yaklaşık Karekök değeri %f",yaklasikKarekok);
     return 0;
 }
