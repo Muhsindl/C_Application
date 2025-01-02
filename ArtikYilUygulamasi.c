89006a14f9cbe8f9afd4996d28792c3085c03de8
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-01-02 18:58:40 +0300

    ArtikYilUygulamasi.c

--- ArtikYilUygulamasi.c
+++ ArtikYilUygulamasi.c
@@ -3,8 +3,10 @@
 int main()
 {
     int yil;
+    // Yıl değerinin kullanıcıdan alınması
     printf("Yıl değeri giriniz: ");
     scanf("%d",&yil);
+    // Artık yıl srogulamasının yapılması
     if((yil%4==0)&&(yil%100!=0)){
         printf("Artık yıldır.");
     }else{
