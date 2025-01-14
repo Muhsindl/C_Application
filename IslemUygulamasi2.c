7d8592e6c936245ca901629be70e650b57450315
Muhsin DOLU <muhsindolu06@gmail.com>
2025-01-14 18:11:00 +0300

    İşlem Uygulaması 2
    Kullanıcı tarafından girilen sayı ve üst sınır aralığında işlem yapan uygulama

---
+++ IslemUygulamasi2.c
@@ -0,0 +1,33 @@
+// Formül: toplam = ∑ (x^i) / (i!)  [i=1'den sayi'ya kadar]
+
+#include <stdio.h>
+#include <math.h>
+
+int fact(int); // Declare ediyorum daha önceden tanımlıyorum fonksiyonumu
+int main()
+{
+    // Kullanıcıdan sayı ve aralık alınması
+    int sayi,x;
+    printf("Sayı  giriniz: ");
+    scanf("%d",&x);
+    
+    printf("Sayı sınırı giriniz: ");
+    scanf("%d",&sayi);
+    
+    // İşlemin yapılması
+    float toplam=0;
+    for(int i=1;i<=sayi; i++){
+        toplam+= (pow(x,i))/(fact(i));
+    }
+    printf("Cevap %f",toplam);
+    return 1;
+}
+    // Faktöriyel hesaplama fonksiyonu
+int fact(int n){
+    if (n==1 || n==0){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
